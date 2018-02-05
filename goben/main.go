4e1cb0570e1d005da9dd98b35f448e190632ccb9
udhos <everton.marques@gmail.com>
2018-02-05 15:20:29 -0200

    Command-line.

--- goben/main.go
+++ goben/main.go
@@ -1,4 +1,45 @@
 package main
 
+import (
+	"flag"
+	"fmt"
+	"log"
+	"strings"
+)
+
+type hostList []string
+
+type config struct {
+	hosts      hostList
+	listenAddr string
+}
+
+func (h *hostList) String() string {
+	return fmt.Sprint(*h)
+}
+
+func (h *hostList) Set(value string) error {
+	for _, hh := range strings.Split(value, ",") {
+		log.Printf("cmd-line host: %s", hh)
+		*h = append(*h, hh)
+	}
+	return nil
+}
 func main() {
+
+	app := config{}
+
+	flag.Var(&app.hosts, "hosts", "comma-separated list of host[:port]")
+	flag.StringVar(&app.listenAddr, "listen", ":80", "listen address host:port")
+
+	flag.Parse()
+
+	log.Printf("listen=%s hosts=%q", app.listenAddr, app.hosts)
+
+	if len(app.hosts) == 0 {
+		log.Printf("server mode")
+		return
+	}
+
+	log.Printf("client mode")
 }
