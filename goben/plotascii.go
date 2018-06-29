b3616fe3702f497976c531a9b0f1167baf89607f
udhos <everton.marques@gmail.com>
2018-06-29 12:57:28 -0300

    Wider asciigraph.

--- goben/plotascii.go
+++ goben/plotascii.go
@@ -8,15 +8,19 @@ import (
 )
 
 func plotascii(info *ExportInfo) {
+
+	height := 10
+	width := 70
+
 	if len(info.Input.YValues) > 0 {
 		log.Println("input:")
-		input := asciigraph.Plot(info.Input.YValues, asciigraph.Caption("Input Mbps"), asciigraph.Height(10), asciigraph.Width(60))
+		input := asciigraph.Plot(info.Input.YValues, asciigraph.Caption("Input Mbps"), asciigraph.Height(height), asciigraph.Width(width))
 		fmt.Println(input)
 	}
 
 	if len(info.Output.YValues) > 0 {
 		log.Println("output:")
-		output := asciigraph.Plot(info.Output.YValues, asciigraph.Caption("Output Mbps"), asciigraph.Height(10), asciigraph.Width(60))
+		output := asciigraph.Plot(info.Output.YValues, asciigraph.Caption("Output Mbps"), asciigraph.Height(height), asciigraph.Width(width))
 		fmt.Println(output)
 	}
 }
