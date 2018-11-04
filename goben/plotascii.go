9ff1fbb1f78e22af234a2c20300a480729536ce4
Justin Hempsey <jhempsey33@gmail.com>
2018-11-04 13:29:58 -0800

    Append remote connection to output files Add %s formatting to output file names. This way, output files do not get overwritten by the different threads and output files can be distinguished by the remote connection. Also append the remote host connection and thread index to the ASCII graph caption to clarify output and be similar to the output file names

--- goben/plotascii.go
+++ goben/plotascii.go
@@ -7,20 +7,22 @@ import (
 	"github.com/guptarohit/asciigraph"
 )
 
-func plotascii(info *ExportInfo) {
+func plotascii(info *ExportInfo, remote string, index int) {
 
 	height := 10
 	width := 70
 
 	if len(info.Input.YValues) > 0 {
-		log.Println("input:")
-		input := asciigraph.Plot(info.Input.YValues, asciigraph.Caption("Input Mbps"), asciigraph.Height(height), asciigraph.Width(width))
+		caption := fmt.Sprintf("Input Mbps: %s Connection %d", remote, index)
+		log.Printf("%s input:", remote)
+		input := asciigraph.Plot(info.Input.YValues, asciigraph.Caption(caption), asciigraph.Height(height), asciigraph.Width(width))
 		fmt.Println(input)
 	}
 
 	if len(info.Output.YValues) > 0 {
-		log.Println("output:")
-		output := asciigraph.Plot(info.Output.YValues, asciigraph.Caption("Output Mbps"), asciigraph.Height(height), asciigraph.Width(width))
+		caption := fmt.Sprintf("Output Mbps: %s Connection %d", remote, index)
+		log.Printf("%s output:", remote)
+		output := asciigraph.Plot(info.Output.YValues, asciigraph.Caption(caption), asciigraph.Height(height), asciigraph.Width(width))
 		fmt.Println(output)
 	}
 }
