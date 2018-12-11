d9738f01f2c0d48d99c9a0bc6b5a07e7cb64354e
udhos <udhos>
2018-12-11 16:27:02 -0200

    Remove stale lines.

--- build.sh
+++ build.sh
@@ -1,11 +1,5 @@
 #!/bin/sh
 
-#go get github.com/wcharczuk/go-chart
-#go get gopkg.in/v2/yaml
-#go get github.com/guptarohit/asciigraph
-
-gobin=~/go/bin
-
 gofmt -s -w ./goben
 go tool fix ./goben
 go tool vet ./goben
